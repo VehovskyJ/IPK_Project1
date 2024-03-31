f5343e4366050f9112f671523476f97533783bc8
Jaroslav Vehovský <jaroslav.vehovsky@gmail.com>
2024-03-31 19:19:35 +0200

    refactor: extract client.Run into a separate function before implementing UDP

--- Program.cs
+++ Program.cs
@@ -9,52 +9,55 @@ static class Program {
 		Cli cli = new();
 		try {
 			cli.Parse(args);
+
+			// Run the client based on the transport protocol
+			if (cli.TransportProtocol == TransportProtocol.Tcp) {
+				Tcp tcp = new Tcp();
+				RunClient(tcp, cli);
+			} else {
+				// TODO: implement udp
+			}
+		} catch (Exception e) {
+			Error.Print(e.Message);
+			Environment.Exit(1);
+		}
+	}
+
+	static void RunClient(Client client, Cli cli) {
+		if (string.IsNullOrEmpty(cli.ServerAddress)) {
+			Error.Print("Missing required arguments: Server Address.");
+			Environment.Exit(1);
+		}
+
+		// Run the client
+		try {
+			client.Run(cli.ServerAddress, cli.ServerPort);
 		} catch (Exception e) {
 			Error.Print(e.Message);
 			Environment.Exit(1);
 		}
 		
-		if (cli.TransportProtocol == TransportProtocol.Tcp) {
-			// Create TCP client
-			Tcp tcp = new Tcp();
-			if (string.IsNullOrEmpty(cli.ServerAddress)) {
-				Error.Print("Missing required arguments: Server Address.");
-				Environment.Exit(1);
-			} else {
-				// Else is not needed, since the program exits on error,
-				// but the warning about possible null persist when else is not present
-				try {
-					tcp.Run(cli.ServerAddress, cli.ServerPort);
-				} catch (Exception e) {
-					Error.Print(e.Message);
-					Environment.Exit(1);
+		// Handle Ctrl+C
+		Console.CancelKeyPress += (sender, eventArgs) => {
+			eventArgs.Cancel = true;
+			client.Close();
+			Environment.Exit(0);
+		};
+		
+		// Read data from the console and send it to the server
+		try {
+			while (true) {
+				string? message = Console.ReadLine();
+				if (!string.IsNullOrEmpty(message)) {
+					client.SendData(message);
+				} else {
+					Error.Print("Input cannot be empty.");
 				}
 			}
-			
-			// Handle Ctrl+C
-			Console.CancelKeyPress += (sender, eventArgs) => {
-				eventArgs.Cancel = true;
-				tcp.Close();
-				Environment.Exit(0);
-			};
-			
-			// Read data from the console
-			try {
-				while (true) {
-					string? message = Console.ReadLine();
-					if (!string.IsNullOrEmpty(message)) {
-						tcp.SendData(message);
-					} else {
-						Error.Print("Input cannot be empty.");
-					}
-				}
-			} catch (Exception e) {
-				Error.Print(e.Message);
-			} finally {
-				tcp.Close();
-			}
-		} else {
-			// TODO: Implement UDP client
+		} catch (Exception e) {
+			Error.Print(e.Message);
+		} finally {
+			client.Close();
 		}
 	}
 }
