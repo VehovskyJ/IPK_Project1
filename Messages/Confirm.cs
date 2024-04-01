b565133b641cf2b38138db8bfa9b10f32937d8ba
Jaroslav Vehovský <jaroslav.vehovsky@gmail.com>
2024-04-01 19:18:20 +0200

    fix: add missing imports

--- Messages/Confirm.cs
+++ Messages/Confirm.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using IPK_Project1.Enums;
 using IPK_Project1.Interfaces;
 
--- Messages/Join.cs
+++ Messages/Join.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Text;
 using IPK_Project1.Enums;
 using IPK_Project1.Interfaces;
